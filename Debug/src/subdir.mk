25e8e512adb984e4ee1f56369dca223f96b398fc
HERNANDEZ HERNANDEZ <alu4404@cc126.(none)>
2013-11-26 12:32:09 +0000

    Cambios menores

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -26,7 +26,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include/SDL2 -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -D_GLIBCXX_PROFILE -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I/usr/include/SDL2 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
